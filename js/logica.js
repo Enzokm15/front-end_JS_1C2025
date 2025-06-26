1c84f90d87bb5d7d75d7ab8059ab4aeb375e36b7
Enzokm15 <enzomarmartin2016@gmail.com>
2025-06-25 23:33:20 -0300

    productos dinamicos

--- js/logica.js
+++ js/logica.js
@@ -6,36 +6,39 @@ function toggleDisplayNone(id) {
 }
 
 function getHtmlProduct(product) {
-  const html =`
-    <div id " "class="product">
-      <h2>${product.nombre}</h2>
-      <p>${product.descripcion}</p>
-      <span>Precio: $${prod.precio}</span>
-    </div>`
+  const html =
+  `
+    <div id="${product.id}" class="column product">
+      <div class="img-product">
+        <img src="${product.imagenes[0]}" alt=""> 
+      </div>
+          
+      <span class="center">"${product.nombre}" </span>   
+      <div class="arrow j-space-a align-center ">
+
+        <span>"${product.precio}"</span>
+
+          <button class="no-background border-round " onclick="addCart('id')">
+          <i class="bi bi-cart-plus-fill"></i>
+                  
+          </button>
+      </div>
+    </div>
+  `
   ;
-  contenedor.insertAdjacentHTML("beforeend", html);
+  return html;
 }
 
 function loadShoppingCart (insertionLocation) {
-    var productos;
-    var location=document.getElementById(insertionLocation)
-    fetch('../assets/datos/productos.json', GET)
-    .then(res =>{
-        productos=res.json();
+    var contenedor=document.getElementById(insertionLocation)
+    fetch('../assets/datos/productos.json')
+    .then(productos =>productos.json())
+    .then(productos => {
+      productos.forEach(producto => contenedor.insertAdjacentHTML("beforeend",getHtmlProduct(producto) ))
     });
     
-    productos.forEach(producto => {
-        let newItem= document.createElement("div");
-        div.classList.add ('product');
-        newItem.appendChild
-        location.appendChild(newItem);
-    });
-
-
-
-
 
 }
 
 
-window.addEventListener("load", loadShoppingCart("containerProduct"));
+window.addEventListener("load", () => loadShoppingCart("containerProducts"));
